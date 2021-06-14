86b3505ed12ff01de6de827b6e31b47a13745df4
James Lea <jameslea@Jamess-MBP.localdomain>
2021-06-14 12:08:42 -0600

    Controllers built out

--- ItsMorphinTime/ItsMorphinTime/Controllers/ViewControllers/MainViewController.swift
+++ ItsMorphinTime/ItsMorphinTime/Controllers/ViewControllers/MainViewController.swift
@@ -7,52 +7,81 @@
 
 import UIKit
 
+protocol displayCell: AnyObject {
+    func showSelectedCell(name: String)
+}
+
 class MainViewController: UIViewController {
     // MARK: - Outlets
-    
     @IBOutlet weak var teamImageView: UIImageView!
     @IBOutlet weak var teamTableView: UITableView!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
+    
+    // MARK: - Properties
+    static weak var delegate: displayCell?
     
     //MARK: - Actions
     @IBAction func redRangerButtonTapped(_ sender: Any) {
-        
+        MainViewController.delegate?.showSelectedCell(name: "James Lea")
     }
     
     @IBAction func blueRangerButtonTapped(_ sender: Any) {
-        
+        MainViewController.delegate?.showSelectedCell(name: "AnthonyByrd")
     }
     
     @IBAction func blackRangerButtonTapped(_ sender: Any) {
-        
+        MainViewController.delegate?.showSelectedCell(name: "Ethan Scott")
     }
     
     @IBAction func pinkRangerButtonTapped(_ sender: Any) {
-        
+        MainViewController.delegate?.showSelectedCell(name: "Lizzie")
     }
     
     @IBAction func yellowRangerButtonTapped(_ sender: Any) {
-        
+        MainViewController.delegate?.showSelectedCell(name: "Colton")
     }
     
     @IBAction func itsMorphinTimeButtonTapped(_ sender: Any) {
         
     }
     
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+
+}
+
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return RangerController.rangers.count
+        return RangerController.rangers.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "rangerCell") as? RangerTableViewCell
+        
+        let ranger = RangerController.rangers[indexPath.row]
+        
+        cell?.ranger = ranger
+        
+        return cell ?? UITableViewCell()
+    }
+    
+    
+     // MARK: - Navigation
+     
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailVC" {
+            guard let destination = segue.destination as? DetailViewController,
+                  let indexPath = teamTableView.indexPathForSelectedRow else {return}
+            
+            let ranger = RangerController.rangers[indexPath.row]
+//            destination.ranger = ranger
+        }
+     }
+     
+    
 }
--- ItsMorphinTime/ItsMorphinTime/Views/CustomViews/RangerTableViewCell.swift
+++ ItsMorphinTime/ItsMorphinTime/Views/CustomViews/RangerTableViewCell.swift
@@ -7,10 +7,34 @@
 
 import UIKit
 
+
+
 class RangerTableViewCell: UITableViewCell {
     // MARK: - Outlets
     @IBOutlet weak var rangerImageView: UIImageView!
+    
+    var ranger: Ranger? {
+        didSet {
+            updateView()
+        }
+    }
+    
+    func updateView(){
+        self.contentView.isHidden = true
+        MainViewController.delegate = self
+    }
+    
+    func showCell(name: String){
+        if name == ranger?.name {
+            self.contentView.isHidden = false
+        }
+    }
 
+}
 
-
+extension RangerTableViewCell: displayCell {
+    func showSelectedCell(name: String) {
+        showCell(name: name)
+    }
+    
 }
